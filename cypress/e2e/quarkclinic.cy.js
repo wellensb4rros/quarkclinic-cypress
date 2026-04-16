e34888db5f31fc3c7ce9da8f5c3f3eb72494c9f6
Wellen Barros <wellensbarros@gmail.com>
2026-04-16 10:11:12 -0300

    codigo operacional

--- cypress/e2e/quarkclinic.cy.js
+++ cypress/e2e/quarkclinic.cy.js
@@ -4,67 +4,42 @@ function gerarEmailUnico() {
 }
 
 describe("QuarkClinic - Testes E2E", () => {
+
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+
   it("Fluxo 1 - Cadastro de Novo Usuário", () => {
     cy.visit("https://agendamento.quarkclinic.com.br/index/363622206");
 
-    cy.contains("Login", { timeout: 10000 }).should("be.visible").click();
-
-    cy.contains("Cadastre-se", { timeout: 10000 }).should("be.visible").click();
+    cy.contains("Login", { timeout: 10000 }).click();
+    cy.contains("Cadastre-se", { timeout: 10000 }).click();
 
     const email = gerarEmailUnico();
     const cpf = "01124457488";
 
-    cy.get('[data-cy="campo-nome-input"]')
-      .filter(":visible")
-      .first()
-      .type("Teste Cypress");
+    cy.get('[data-cy="campo-nome-input"]').filter(":visible").first().type("Teste Cypress");
 
-    cy.get('[data-cy="campo-telefone-input"]')
-      .filter(":visible")
-      .first()
-      .type("84999999999");
+    cy.get('[data-cy="campo-telefone-input"]').filter(":visible").first().type("84999999999");
 
-    cy.get('[data-cy="campo-sexo-select"]')
-      .filter(":visible")
-      .first()
-      .select("FEMININO");
+    cy.get('[data-cy="campo-sexo-select"]').filter(":visible").first().select("FEMININO");
 
-    cy.get('[data-cy="campo-data-nascimento-input"]')
-      .filter(":visible")
-      .first()
-      .type("10/10/2000");
+    cy.get('[data-cy="campo-data-nascimento-input"]').filter(":visible").first().type("10/10/2000");
 
-    cy.get('[data-cy="campo-email-input"]')
-      .filter(":visible")
-      .first()
-      .type(email);
+    cy.get('[data-cy="campo-email-input"]').filter(":visible").first().type(email);
 
-    cy.get('[data-cy="campo-tipo-documento-select"]')
-      .filter(":visible")
-      .first()
-      .select("CPF");
+    cy.get('[data-cy="campo-tipo-documento-select"]').filter(":visible").first().select("CPF");
 
-    cy.get('[data-cy="campo-numero-documento-input"]')
-      .filter(":visible")
-      .first()
-      .type(cpf);
+    cy.get('[data-cy="campo-numero-documento-input"]').filter(":visible").first().type(cpf);
 
-    cy.get('[data-cy="campo-senha-input"]')
-      .filter(":visible")
-      .first()
-      .type("violet01");
+    cy.get('[data-cy="campo-senha-input"]').filter(":visible").first().type("violet01");
 
-    cy.get('[data-cy="campo-confirmar-senha-input"]')
-      .filter(":visible")
-      .first()
-      .type("violet01");
+    cy.get('[data-cy="campo-confirmar-senha-input"]').filter(":visible").first().type("violet01");
 
     cy.get("#cb-cadastro").check({ force: true });
 
-    cy.get('[data-cy="btn-criar-conta"]')
-      .filter(":visible")
-      .first()
-      .click();
+    cy.get('[data-cy="btn-criar-conta"]').filter(":visible").first().click();
 
     cy.contains("Consulta Presencial", { timeout: 15000 }).should("be.visible");
   });
@@ -72,71 +47,25 @@ describe("QuarkClinic - Testes E2E", () => {
   it("Fluxo 2 - Login com usuário existente", () => {
     cy.visit("https://agendamento.quarkclinic.com.br/index/363622206");
 
-    cy.contains("Login", { timeout: 10000 }).should("be.visible").click();
-
-    cy.get('[data-cy="campo-usuario-input"]')
-      .filter(":visible")
-      .first()
-      .type("violet_s@gmail.com");
-
-    cy.get('[data-cy="campo-senha-input"]')
-      .filter(":visible")
-      .first()
-      .type("violet01");
-
-    cy.get('[data-cy="btn-submit-login"]')
-      .filter(":visible")
-      .first()
-      .click();
-
-    cy.contains("Consulta Presencial", { timeout: 10000 }).should("be.visible");
+    cy.login("violet_s@gmail.com", "violet01");
   });
 
   it("Fluxo 3 - Agendamento de Consulta Presencial", () => {
     cy.visit("https://agendamento.quarkclinic.com.br/index/363622206");
 
-    cy.contains("Login", { timeout: 10000 }).should("be.visible").click();
-
-    cy.get('[data-cy="campo-usuario-input"]')
-      .filter(":visible")
-      .first()
-      .type("violet_s@gmail.com");
-
-    cy.get('[data-cy="campo-senha-input"]')
-      .filter(":visible")
-      .first()
-      .type("violet01");
-
-    cy.get('[data-cy="btn-submit-login"]')
-      .filter(":visible")
-      .first()
-      .click();
-
-    cy.get("body").then(($body) => {
-      if ($body.find('[data-cy="modal-login-btn-fechar"]').length > 0) {
-        cy.get('[data-cy="modal-login-btn-fechar"]').click();
-      }
-    });
+    cy.login("violet_s@gmail.com", "violet01");
 
     cy.contains("Consulta Presencial", { timeout: 15000 })
       .should("be.visible")
       .click();
 
-    cy.contains("PARTICULAR", { timeout: 15000 })
-      .should("be.visible")
-      .click();
+    cy.contains("PARTICULAR", { timeout: 15000 }).click();
 
-    cy.contains("CARDIOLOGIA", { timeout: 15000 })
-      .should("be.visible")
-      .click();
+    cy.contains("CARDIOLOGIA", { timeout: 15000 }).click();
 
-    cy.get("label", { timeout: 15000 })
-      .contains("CLINICA")
-      .should("be.visible")
-      .click();
+    cy.get("label").contains("CLINICA").click();
 
-
-    cy.get('[data-cy^="agenda-item-horario-texto"]', { timeout: 15000 })
+    cy.get('[data-cy^="agenda-item-horario-texto"]')
       .filter(":visible")
       .first()
       .invoke("text")
@@ -144,67 +73,27 @@ describe("QuarkClinic - Testes E2E", () => {
         cy.wrap(horarioSelecionado.trim()).as("horarioSelecionado");
       });
 
-
-    cy.get('[data-cy^="agenda-item-horario-texto"]', { timeout: 15000 })
+    cy.get('[data-cy^="agenda-item-horario-texto"]')
       .filter(":visible")
       .first()
       .click();
 
-  
-    cy.get("body", { timeout: 15000 }).then(($body) => {
-      if ($body.text().includes("Identifique-se")) {
-
-        cy.get('[data-cy="campo-usuario-input"]', { timeout: 15000 })
-          .filter(":visible")
-          .first()
-          .clear()
-          .type("violet_s@gmail.com");
-
-        cy.get('[data-cy="campo-senha-input"]', { timeout: 15000 })
-          .filter(":visible")
-          .first()
-          .clear()
-          .type("violet01");
-
-      
-        cy.contains("Eu concordo com a Política de Privacidade", { timeout: 15000 })
-          .should("be.visible")
-          .click();
-
-        cy.get('[data-cy="btn-submit-login"]', { timeout: 15000 })
-          .filter(":visible")
-          .first()
-          .click();
-        
-        cy.url({ timeout: 15000 }).should("include", "/paciente");
-        cy.contains("Selecione o paciente", { timeout: 15000 }).should("be.visible");
-
-      }
-    });
-
-  
     cy.contains("Paciente", { timeout: 15000 }).should("be.visible");
 
-  
-    cy.get("label", { timeout: 15000 })
-      .filter(":visible")
-      .first()
-      .click();
+    cy.get("label").filter(":visible").first().click();
 
-  
     cy.contains("Confirmação", { timeout: 15000 }).should("be.visible");
 
     cy.contains("CARDIOLOGIA", { timeout: 15000 }).should("be.visible");
 
     cy.get("@horarioSelecionado").then((horario) => {
-      cy.contains(horario, { timeout: 15000 }).should("be.visible");
+      cy.contains(horario).should("be.visible");
     });
 
-    cy.contains("Confirmar Agendamento", { timeout: 15000 })
-      .should("be.visible")
-      .click();
+    cy.contains("Confirmar Agendamento").click();
 
     cy.contains(/Agendamento efetuado com sucesso!/i, { timeout: 20000 })
       .should("be.visible");
   });
+
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,22 +1,38 @@
-Cypress.Commands.add("login", (email, senha) => {
-  cy.visit("https://agendamento.quarkclinic.com.br/index/363622206");
-
+Cypress.Commands.add('login', (usuario, senha) => {
   cy.contains("Login", { timeout: 10000 })
     .should("be.visible")
     .click();
 
-  cy.get('[data-cy="campo-usuario-input"]', { timeout: 10000 })
-    .should("be.visible")
-    .type(email);
+  cy.get('[data-cy="campo-usuario-input"]')
+    .filter(":visible")
+    .first()
+    .type(usuario);
 
   cy.get('[data-cy="campo-senha-input"]')
-    .should("be.visible")
+    .filter(":visible")
+    .first()
     .type(senha);
 
   cy.get('[data-cy="btn-submit-login"]')
-    .should("be.visible")
+    .filter(":visible")
+    .first()
     .click();
 
-  cy.contains("Consulta Presencial", { timeout: 10000 })
+  // 🔥 Tratamento da política de privacidade (caso apareça)
+  cy.get("body").then(($body) => {
+    if ($body.text().includes("Política de Privacidade")) {
+      cy.contains("Eu concordo com a Política de Privacidade")
+        .should("be.visible")
+        .click();
+
+      cy.get('[data-cy="btn-submit-login"]')
+        .filter(":visible")
+        .first()
+        .click();
+    }
+  });
+
+  // valida login concluído
+  cy.contains("Consulta Presencial", { timeout: 15000 })
     .should("be.visible");
 });
